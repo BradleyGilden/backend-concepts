a6afe36901cb8c553bc3d80aa2e350ab91a2610a
Bradley Gilden <bradleygilden@gmail.com>
2023-12-23 15:15:48 +0000

    Sat, Dec 23, 2023, 5:15 PM +02:00

--- Nodejs/fileHandling/main.js
+++ Nodejs/fileHandling/main.js
@@ -1,9 +1,13 @@
 const fs = require('fs');
 
 try {
+	// synchronous reading
 	const data = fs.readFileSync('./txt/input.txt', 'utf-8');
 	console.log(data);
-	fs.writeFileSync('./txt/output.txt', 'this is being written to a file')
+	// synchronous writing
+	fs.writeFileSync('./txt/output.txt', 'this is being written to a file');
+	// synchronous appending
+	fs.writeFileSync('./txt/output.txt', '\nthis is being appended to the file', {'encoding': 'utf-8', 'flag': 'a'});
 } catch (err) {
 	console.log(err.message);
 }
