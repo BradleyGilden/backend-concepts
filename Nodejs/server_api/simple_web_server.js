077d647b7dfd85acc8c517166ebe8900c698e2ec
Bradley Gilden <bradleygilden@gmail.com>
2023-12-23 17:13:26 +0000

    Sat, Dec 23, 2023, 7:13 PM +02:00

--- Nodejs/server_api/simple_web_server.js
+++ Nodejs/server_api/simple_web_server.js
@@ -1,8 +1,15 @@
 const http = require('http');
-const url = require('url');const server = http.createServer((req, res) => {
-	res.end('<h1>Hello from the web server</h1>');
+const url = require('url');
+
+const server = http.createServer((req, res) => {
+	const pathName = req.url;
+	if (pathName === '/' || pathName === '/overview') {
+		res.end('<h1>This is the overview </h1>');
+	} else if (pathName === '/product') {
+		res.end('<h1>This is the product </h1>');
+	}
 });
 
 server.listen(8000, 'localhost', () => {
 	console.log('Listening on port on localhost:8000');
-});
+})
