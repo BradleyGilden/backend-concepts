20e737406fef506ce58377ae0b179fe8cb928226
Bradley Gilden <bradleygilden@gmail.com>
2023-12-23 17:07:05 +0200

    file handling

--- Nodejs/fileHandling/main.js
+++ Nodejs/fileHandling/main.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 
 try {
-	
+	const data = fs.readFileSync('./txt/input.txt', 'utf-8');
+	console.log(data);
+} catch (err) {
+	console.log(err.message);
 }
-fs.readFileSync('./txt/input.txt', 'utf-8');
