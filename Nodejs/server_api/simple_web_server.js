ef5640104a999f574c188e808a8848b5aad5dffa
Bradley Gilden <bradleygilden@gmail.com>
2023-12-23 17:45:12 +0000

    Sat, Dec 23, 2023, 7:45 PM +02:00

--- Nodejs/server_api/simple_web_server.js
+++ Nodejs/server_api/simple_web_server.js
@@ -3,14 +3,20 @@ const url = require('url');
 
 const server = http.createServer((req, res) => {
 	const pathName = req.url;
-	res.writeHead(404, {
-		'Content-Type': 'text/html'
-	});
 	if (pathName === '/' || pathName === '/overview') {
+		res.writeHead(404, {'Content-Type': 'text/html'});
 		res.end('<h2 align="center">This is the overview</h2>');
 	} else if (pathName === '/product') {
+		res.writeHead(404, {'Content-Type': 'text/html'});
 		res.end('<h2 align="center">This is the product</h2>');
+	} else if(pathName === '/api') {
+		fs.readFile('./api_data.json', 'utf-8', (err, data) =>{
+			const jsonData = JSON.parse(data);
+			res.writeHead(404, {'Content-Type': 'application/json'});
+			res.end(jsonData);
+		})
 	} else {
+		res.writeHead(404, {'Content-Type': 'text/html'});
 		res.end('<h1 align="center">404</h1><br/><h2 align="center">Page not found</h2>')
 	}
 });
