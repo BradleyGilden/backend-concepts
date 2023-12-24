4315857213d55a0b28e1a7c56508ce5ee87cc7d7
Bradley Gilden <bradleygilden@gmail.com>
2023-12-24 09:42:24 +0000

    Sun, Dec 24, 2023, 11:42 AM +02:00

--- Nodejs/server_api/simple_web_server.js
+++ Nodejs/server_api/simple_web_server.js
@@ -1,11 +1,27 @@
-const {readFile, writeFile} = require('fs');
+const fs = require('fs');
 const http = require('http');
+const url = require('url');
 
 const server = http.createServer((req, res) => {
-	res.write('Welcome to the Home Page')
-	res.end()
+	const pathName = req.url;
+	if (pathName === '/' || pathName === '/overview') {
+		res.writeHead(200, {'Content-Type': 'text/html'});
+		res.end('<h2 align="center">This is the overview</h2>');
+	} else if (pathName === '/product') {
+		res.writeHead(200, {'Content-Type': 'text/html'});
+		res.end('<h2 align="center">This is the product</h2>');
+	} else if(pathName === '/api') {
+		fs.readFile('./api_data.json', 'utf-8', (err, data) =>{
+			const jsonData = JSON.parse(data);
+			res.writeHead(200, {'Content-Type': 'application/json'});
+			res.end(JSON.stringify(jsonData));
+		})
+	} else {
+		res.writeHead(404, {'Content-Type': 'text/html'});
+		res.end('<h1 align="center">404</h1><br/><h2 align="center">Page not found</h2>')
+	}
 });
 
-server.listen(5500, 'localhost', () => {
+server.listen(8000, 'localhost', () => {
 	console.log('Listening on port on localhost:8000');
 })
