a1ebc0d8b5f37f8338c6df8576e498513d0d859f
BradleyGilden <bradleygilden@gmail.com>
2024-01-06 11:27:33 +0200

    fixed linting issues

--- Nodejs/file_handling/async.js
+++ Nodejs/file_handling/async.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
-// asynchronous functions allow the main program to continue executing commands if the function takes
-// too long to return a value or do an action
+// asynchronous functions allow the main program to continue executing commands
+// if the function takes too long to return a value or do an action
 
 // asynchronous reading using async and await
 const reading = async () => {
@@ -12,5 +12,5 @@ reading();
 // using promises
 fs.readFile('./txt/input.txt', 'utf-8', (err, data) => {
   console.log(data);
-})
+});
 console.log('after read data\n');
--- Nodejs/file_handling/sync.js
+++ Nodejs/file_handling/sync.js
@@ -7,7 +7,7 @@ try {
   // synchronous writing
   fs.writeFileSync('./txt/output.txt', 'this is being written to a file');
   // synchronous appending
-  fs.writeFileSync('./txt/output.txt', '\nthis is being appended to the file', {'encoding': 'utf-8', 'flag': 'a'});
+  fs.writeFileSync('./txt/output.txt', '\nthis is being appended to the file', { encoding: 'utf-8', flag: 'a' });
 } catch (err) {
   console.log(err.message);
 }
