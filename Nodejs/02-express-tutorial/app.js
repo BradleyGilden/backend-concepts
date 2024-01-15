2ecdf0efaa047e80a8f30ff3db72721566d5518a
BradleyGilden <bradleygilden@gmail.com>
2024-01-15 16:56:45 +0200

    doc update

--- Nodejs/02-express-tutorial/app.js
+++ Nodejs/02-express-tutorial/app.js
@@ -1,1 +1,16 @@
-console.log('Express Tutorial')
+const express = require('express');
+const path = require('path');
+const app = express();
+
+
+app.get('/', (req, res) => {
+  res.json({ name: 'Bradley', surname: 'Gilden' })
+});
+
+app.all('*', (req, res) => {
+  res.status(404).send('resource not found');
+});
+
+app.listen(5500, () => {
+  console.log('server is listening on port 5500');
+});
